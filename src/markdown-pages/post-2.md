49b1275bf3ede9e30b2724146e0052247dbda2be
Noah Burroughs <noahsburroughs@gmail.com>
2021-10-04 16:23:48 -0500

    Initial commit

---
+++ src/markdown-pages/post-2.md
@@ -0,0 +1,7 @@
+---
+slug: "/blog/my-first-post"
+date: "2020-05-07"
+title: "My second blog post"
+---
+
+Hello Universe!
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby blog with Markdown pages.</p>
+    <p>
+      <Link to="/blog/my-first-post/">Go to my first Markdown blog post</Link>
+    </p>
+  </Layout>
+)
+
+export default IndexPage
